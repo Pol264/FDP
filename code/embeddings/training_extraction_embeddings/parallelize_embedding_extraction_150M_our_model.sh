788135e3accb9857bc06e20e4982439d609eaf7b
Pol264 <144927137+Pol264@users.noreply.github.com>
2025-06-10 15:59:53 +0200

    Add files via upload

---
+++ code/embeddings/training_extraction_embeddings/parallelize_embedding_extraction_150M_our_model.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --job-name=parallelize_job_%A_%a
+#SBATCH --output=/leonardo_work/EUHPC_A05_043/TFG_pjardi/outputs/parallelize_job_%A_%a.out
+#SBATCH --error=/leonardo_work/EUHPC_A05_043/TFG_pjardi/outputs/parallelize_job_%A_%a.err
+#SBATCH --time=1-00:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=3
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=24000
+#SBATCH --gres=gpu:3
+#SBATCH --account=EUHPC_A05_043
+#SBATCH --partition=boost_usr_prod
+#SBATCH --qos=normal
+#SBATCH --array=0-9  # Launch 10 jobs (adjust based on N_JOBS)
+
+module load cuda/12.1
+module load python/3.10.8--gcc--8.5.0
+
+export WANDB_API_KEY=70b773919c9a6f9239c023530b128c900b8963bb
+
+DATA_PATH="/leonardo_work/EUHPC_A05_043/TFG_pjardi/uniref50_dataset/uniref50_without_validation_sequences.fasta.gz"
+OUTPUT_DIR="/leonardo_scratch/large/userexternal/pjardiya/embeddings/embeddings_150M_our_model"
+TOTAL_SEQS=67791787  # total sequences in dataset
+N_JOBS=10  # Adjust total number of parallel jobs
+JOB_IDX=${SLURM_ARRAY_TASK_ID}
+
+python parallelize_embedding_extraction_pol_150M_our_model.py \
+    --data_path $DATA_PATH \
+    --output_dir $OUTPUT_DIR \
+    --total_seqs $TOTAL_SEQS \
+    --job_idx $JOB_IDX \
+    --n_jobs $N_JOBS
+
